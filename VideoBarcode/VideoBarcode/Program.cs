40d59f6ef7b0f29a27bc920ca758a52a15dab419
Connor Greene <connor@connorgreene.net>
2024-10-22 01:19:36 -0400

    Sample one frame per second instead of processing each frame

--- VideoBarcode/VideoBarcode/Program.cs
+++ VideoBarcode/VideoBarcode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json;
+using System.Text.Json.Serialization;
 using OpenCvSharp;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Drawing.Processing;
@@ -7,10 +8,35 @@ using SixLabors.ImageSharp.Processing;
 
 namespace VideoBarcode;
 
+class FrameColorData
+{
+    [JsonPropertyName("P")]
+    public uint PackedBgra32 { get; set; }
+
+    [JsonPropertyName("T")]
+    public int TimestampMsec { get; set; }
+
+    [JsonIgnore]
+    internal Bgra32 Bgra32 => new() { PackedValue = PackedBgra32 };
+
+    public FrameColorData()
+    {
+
+    }
+
+    internal FrameColorData(Color color, int timestampMsec)
+    {
+        PackedBgra32 = color.ToPixel<Bgra32>().PackedValue;
+        TimestampMsec = timestampMsec;
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
+        int sampleMsec = 1000;
+
         string directory = @"/Users/Connor/Desktop/Her";
 
         string videoFilePath = Path.Combine(directory, "Her (2013).mp4");
@@ -21,17 +47,17 @@ class Program
         var capture = new VideoCapture(videoFilePath);
 
         Console.WriteLine($"Processing {Path.GetFileName(videoFilePath)}...");
+        Console.WriteLine($"Frame Rate: {capture.Fps} FPS, Sampling every {sampleMsec} ms");
         Console.WriteLine($"Duration: {TimeSpan.FromSeconds(capture.FrameCount / capture.Fps)}");
 
-        List<Color> averageColors;
+        List<FrameColorData> averageColors;
 
         // use the JSON file of average colors if it exists, otherwise compute a new list from the capture
         if (File.Exists(jsonFilePath))
         {
             // ImageSharp packs colors from LSB to MSB. Bgra32 is 0xARGB, Argb32 is 0xBGRA, Rgba32 is 0xABGR, etc.
             // Hence the file convention of Title.argb.uint.json for the packed format of Bgra32
-            averageColors = JsonSerializer.Deserialize<List<uint>>(File.ReadAllText(jsonFilePath))
-                ?.Select(u => new Color(new Bgra32 { PackedValue = u })).ToList()
+            averageColors = JsonSerializer.Deserialize<List<FrameColorData>>(File.ReadAllText(jsonFilePath))
                 ?? throw new InvalidOperationException("Failed to deserialize JSON file with color data");
         }
         else
@@ -41,129 +67,101 @@ class Program
                 throw new InvalidOperationException($"{videoFilePath} not found");
             }
 
-            averageColors = AverageColorsOfCapture(capture);
+            averageColors = AverageColorsOfCapture(capture, sampleMsec);
 
             // Serialize the average colors list and write it to a file.
             // Note that this is the "packed" color format used by ImageSharp, different from the bit-shifted ARGB int that System.Drawing.Color uses.
             // From MSB to LSB, Bgra32 will be packed into the uint as 0xARGB. LSB to MSB is 0xBGRA, hence the type name.
-            File.WriteAllText(jsonFilePath, JsonSerializer.Serialize(averageColors.Select(c => c.ToPixel<Bgra32>().PackedValue).ToArray()));
+            File.WriteAllText(jsonFilePath, JsonSerializer.Serialize(averageColors));
         }
 
-        var summarizedColors = SummarizeColorsByTime(averageColors, Convert.ToInt32(Math.Round(capture.Fps)));
+        var summarizedColors = SummarizeColorsByTime(averageColors);
 
         WriteHistogram(gradientFilePath, summarizedColors, summarizedColors.Length / 4, summarizedColors.Length);
 
         Console.WriteLine("\nFinished.");
     }
 
-    private static List<Color> AverageColorsOfCapture(VideoCapture capture)
+    private static List<FrameColorData> AverageColorsOfCapture(VideoCapture capture, int sampleMsec)
     {
         // this list represents the average color of every frame of the video
-        var averageColors = new List<Color>();
+        var averageColors = new List<FrameColorData>();
 
-        // frame image buffers
-        var images = new Mat<Vec3b>[Environment.ProcessorCount].Select(i => new Mat<Vec3b>()).ToArray();
-
-        int frameIdx = 0;
-        bool finished = false;
-
-        // the list of tasks to find the average color of a frame, one for each logical processor
-        var tasks = new List<Task<Color>>();
-
+        var image = new Mat<Vec3b>();
         while (true)
         {
             // read one frame for each thread
-            for (int i = 0; i < Environment.ProcessorCount; i++)
-            {
-                capture.Read(images[i]);
+            capture.Read(image);
 
-                if (images[i].Empty())
-                {
-                    finished = true;
-                    break;
-                }
-
-                tasks.Add(AverageFrameColorHSV(images[i]));
-
-                frameIdx++;
-            }
-
-            // schedule all of the average frame color tasks
-            Task.WaitAll([.. tasks]);
-
-            // add the results to the running list of average colors
-            averageColors.AddRange(tasks.Select(t => t.Result));
-
-            if (finished)
+            if (image.Empty())
             {
                 break;
             }
 
-            // display progress, formatted to two decimal places
-            float progress = (float)frameIdx / capture.FrameCount;
-            Console.Write($"\rProgress: {string.Format("{0:0.00}", progress * 100.0)}%");
+            averageColors.Add(new FrameColorData(AverageFrameColorHSV(image), capture.PosMsec));
 
-            tasks.Clear();
+            capture.PosMsec += sampleMsec;
+
+            // display progress, formatted to two decimal places
+            float progress = (float)Math.Min(capture.PosFrames, capture.FrameCount) / capture.FrameCount;
+            Console.Write($"\rProgress: {string.Format("{0:0.00}", progress * 100.0)}% {TimeSpan.FromMilliseconds(capture.PosMsec)}");
         }
 
         return averageColors;
     }
 
-    private static Task<Color> AverageFrameColorHSV(Mat<Vec3b> image)
+    private static Color AverageFrameColorHSV(Mat<Vec3b> image)
     {
-        return Task.Run(() =>
+        // accumulators for hue, saturation, and value
+        var hueCounts = new int[360];
+        var saturationAccum = new float[360];
+        var valueAccum = new float[360];
+
+        int hue;
+        float saturation, value;
+
+        var indexer = image.GetIndexer();
+
+        for (int y = 0; y < image.Height; y++)
         {
-            // accumulators for hue, saturation, and value
-            var hueCounts = new int[360];
-            var saturationAccum = new float[360];
-            var valueAccum = new float[360];
-
-            int hue;
-            float saturation, value;
-
-            var indexer = image.GetIndexer();
-
-            for (int y = 0; y < image.Height; y++)
+            for (int x = 0; x < image.Width; x++)
             {
-                for (int x = 0; x < image.Width; x++)
-                {
-                    Vec3b pixel = indexer[y, x];
+                Vec3b pixel = indexer[y, x];
 
-                    // var color = Color.FromArgb(pixel.Item2, pixel.Item1, pixel.Item0);
-                    // ColorHelp.ColorToHSV(color, out double dHue, out saturation, out value);
+                // var color = Color.FromArgb(pixel.Item2, pixel.Item1, pixel.Item0);
+                // ColorHelp.ColorToHSV(color, out double dHue, out saturation, out value);
 
-                    ColorHelp.RGBtoHSV(pixel.Item2, pixel.Item1, pixel.Item0, out float h, out float s, out float v);
+                ColorHelp.RGBtoHSV(pixel.Item2, pixel.Item1, pixel.Item0, out float h, out float s, out float v);
 
-                    // Use cast instead of Convert because Convert will round up to 360 (out of bounds)
-                    hue = (int)h;
+                // Use cast instead of Convert because Convert will round up to 360 (out of bounds)
+                hue = (int)h;
 
-                    hueCounts[hue] += 1;
-                    saturationAccum[hue] += s;
-                    valueAccum[hue] += v;
-                }
+                hueCounts[hue] += 1;
+                saturationAccum[hue] += s;
+                valueAccum[hue] += v;
             }
+        }
 
-            // smooth out the hue array by adding neigboring hues
-            var smoothHueCounts = SmoothArray(hueCounts, 10);
+        // smooth out the hue array by adding neigboring hues
+        var smoothHueCounts = SmoothArray(hueCounts, 10);
 
-            // compute the average hue, saturation, and value
-            hue = Array.IndexOf(smoothHueCounts, smoothHueCounts.Max());
-            saturation = saturationAccum[hue] / hueCounts[hue];
-            value = valueAccum[hue] / hueCounts[hue];
+        // compute the average hue, saturation, and value
+        hue = Array.IndexOf(smoothHueCounts, smoothHueCounts.Max());
+        saturation = saturationAccum[hue] / hueCounts[hue];
+        value = valueAccum[hue] / hueCounts[hue];
 
-            if (double.IsNaN(saturation))
-            {
-                saturation = 0;
-            }
+        if (double.IsNaN(saturation))
+        {
+            saturation = 0;
+        }
 
-            if (double.IsNaN(value))
-            {
-                value = 0;
-            }
+        if (double.IsNaN(value))
+        {
+            value = 0;
+        }
 
-            ColorHelp.HSVtoRGB(out float r, out float g, out float b, hue, saturation, value);
-            return Color.FromRgb((byte)r, (byte)g, (byte)b);
-        });
+        ColorHelp.HSVtoRGB(out float r, out float g, out float b, hue, saturation, value);
+        return Color.FromRgb((byte)r, (byte)g, (byte)b);
     }
 
     private static int[] SmoothArray(int[] inputArr, int factor)
@@ -181,16 +179,17 @@ class Program
         return outputArr;
     }
 
-    private static Color[] SummarizeColorsByTime(List<Color> colors, int fps)
+    private static Color[] SummarizeColorsByTime(List<FrameColorData> frameData)
     {
-        int totalFrames = colors.Count;
-        int numGroups = Convert.ToInt32(Math.Ceiling((double)totalFrames / fps));
+        int totalFrames = frameData.Count;
+        int numGroups = (frameData.Last().TimestampMsec / 1_000) + 1;
 
-        var colorGroups = new List<Bgra32>[numGroups].Select(s => new List<Bgra32>()).ToArray();
+        var colorGroups = Enumerable.Range(0, numGroups).Select(_ => new List<Bgra32>()).ToArray();
 
-        for (int i = 0; i < totalFrames; i++)
+        foreach (FrameColorData entry in frameData)
         {
-            colorGroups[i / fps].Add(colors[i].ToPixel<Bgra32>());
+            int second = entry.TimestampMsec / 1_000;
+            colorGroups[second].Add(entry.Bgra32);
         }
 
         return colorGroups.Select(g =>
